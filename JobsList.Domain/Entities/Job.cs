9f80a9a505b1eff98a7a1465275b3607e9342420
flavio <flavi@DESKTOP-USTCPSC>
2024-05-28 21:51:45 -0300

    refactor: Add update method and create validate domain

--- JobsList.Domain/Entities/Job.cs
+++ JobsList.Domain/Entities/Job.cs
@@ -1,21 +1,48 @@
-﻿namespace JobsList.Domain.Entities
+﻿using JobsList.Domain.Validation;
+
+namespace JobsList.Domain.Entities
 {
-    public sealed class Job(string title, string description, string location, decimal salary) : BaseEntity
+    public sealed class Job : BaseEntity
     {
-        public string Title { get; private set; } = title;
-        public string Description { get; private set; } = description;
-        public string Location { get; private set; } = location;
-        public decimal Salary { get; private set; } = salary;
-        public bool IsActive { get; private set; } = false;
+        public Job(string title, string description, string location, decimal salary)
+        {
+            ValidateDomain(title, description, location, salary);
+            Title = title;
+            Description = description;
+            Location = location;
+            Salary = salary;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Location { get; private set; }
+        public decimal Salary { get; private set; }
+        public bool IsActive { get; private set; }
 
         public void Active()
         {
             IsActive = true;
         }
-
         public void DeActive()
         {
             IsActive = false;
+        }
+
+        public void Update(string title, string description, string location, decimal salary)
+        {
+            ValidateDomain(title, description, location, salary);
+            Title = title;
+            Description = description;
+            Location = location;
+            Salary = salary;
+        }
+
+        public void ValidateDomain(string title, string description, string location, decimal salary)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(title), "Title is required");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Description is required");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(location), "Location is required");
+            DomainExceptionValidation.When(salary < 0, "Salary must be greater than 0");
         }
     }
 }
