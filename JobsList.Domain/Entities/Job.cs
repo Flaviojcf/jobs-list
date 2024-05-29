d66b30bc245f8b6a66a13f9103462e182e98bbfe
flavio <flavi@DESKTOP-USTCPSC>
2024-05-28 21:36:32 -0300

    feat: Create base and job entities

---
+++ JobsList.Domain/Entities/Job.cs
@@ -0,0 +1,21 @@
+﻿namespace JobsList.Domain.Entities
+{
+    public sealed class Job(string title, string description, string location, decimal salary) : BaseEntity
+    {
+        public string Title { get; private set; } = title;
+        public string Description { get; private set; } = description;
+        public string Location { get; private set; } = location;
+        public decimal Salary { get; private set; } = salary;
+        public bool IsActive { get; private set; } = false;
+
+        public void Active()
+        {
+            IsActive = true;
+        }
+
+        public void DeActive()
+        {
+            IsActive = false;
+        }
+    }
+}
