cb04501bf9d2911f857099c6ffa1121f3075289f
flavio <flavi@DESKTOP-USTCPSC>
2024-05-28 22:28:10 -0300

    feat: Create db context configuration

---
+++ JobsList.Infrastructure/Persistance/Configuration/JobsConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using JobsList.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobsList.Infrastructure.Persistance.Configuration
+{
+    public class JobsConfiguration : IEntityTypeConfiguration<Jobs>
+    {
+        public void Configure(EntityTypeBuilder<Jobs> builder)
+        {
+            builder.HasKey(j => j.Id);
+        }
+    }
+}
