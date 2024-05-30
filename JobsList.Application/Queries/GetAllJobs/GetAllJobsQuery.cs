bb56aae5a77c059a42444c156f80321d1620e4bf
flavio <flavi@DESKTOP-USTCPSC>
2024-05-30 20:27:41 -0300

    feat: Create jobs queries

---
+++ JobsList.Application/Queries/GetAllJobs/GetAllJobsQuery.cs
@@ -0,0 +1,10 @@
+﻿using JobsList.Domain.Entities;
+using MediatR;
+
+namespace JobsList.Application.Queries.GetAllJobs
+{
+    public class GetAllJobsQuery(string query) : IRequest<List<Jobs>>
+    {
+        public string Query { get; private set; } = query;
+    }
+}
