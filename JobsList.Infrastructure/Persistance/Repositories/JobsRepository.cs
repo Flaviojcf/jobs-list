c41fcfe98681949ddccc238645f74ca49f6513e7
flavio <flavi@DESKTOP-USTCPSC>
2024-05-30 20:14:37 -0300

    feat: Create job repository

---
+++ JobsList.Infrastructure/Persistance/Repositories/JobsRepository.cs
@@ -0,0 +1,33 @@
+﻿using JobsList.Domain.Entities;
+using JobsList.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobsList.Infrastructure.Persistance.Repositories
+{
+    public class JobsRepository(JobsListDbContext dbContext) : IJobsRepository
+    {
+        private readonly JobsListDbContext _dbContext = dbContext;
+
+        public async Task CreateAsync(Jobs job)
+        {
+            await _dbContext.Jobs.AddAsync(job);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IList<Jobs>> GetAllAsync()
+        {
+            return await _dbContext.Jobs.Where(j => j.IsActive).ToListAsync();
+        }
+
+        public async Task<Jobs> GetByIdAsync(int id)
+        {
+            return await _dbContext.Jobs.SingleOrDefaultAsync(j => j.Id == id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
